efb7de71a58715ba6daa15b9e3bfd0f09fe1fd3c
sachinraut123 <sachinraut13744@gmail.com>
2026-07-01 09:00:32 +0530

    Time: 1 ms (22.43%), Space: 43.1 MB (97.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,36 @@
+
 class Solution {
     public void sortColors(int[] nums) {
         
-        
+        ArrayList<Integer> a=new ArrayList<>();
+        ArrayList<Integer> b=new ArrayList<>();
+        ArrayList<Integer> c=new ArrayList<>();
+
         for(int i=0; i<nums.length; i++){
-            int min=i;
-            for(int j=i+1; j<nums.length; j++){
-
-            if(nums[min]>nums[j]){
-                min=j;
-
+            if(nums[i]==0){
+                a.add(nums[i]);
             }
+            if(nums[i]==1){
+                b.add(nums[i]);
             }
-            int temp=nums[i];
-            nums[i]=nums[min];
-            nums[min]=temp;
+            if(nums[i]==2){
+                c.add(nums[i]);
+            }
         }
+        int index=0;
+        for(int i=0; i<a.size(); i++){
+            nums[index++]=a.get(i);
+
+        }
+        for(int i=0; i<b.size(); i++){
+            nums[index++]=b.get(i);
+
+        }
+        for(int i=0; i<c.size(); i++){
+            nums[index++]=c.get(i);
+
+        }
+        
     }
 }
+    
