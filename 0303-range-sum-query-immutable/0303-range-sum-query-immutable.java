72252921b9658ece8e400ea0ee294de5477596e2
sachinraut123 <sachinraut13744@gmail.com>
2026-07-14 08:35:01 +0530

    Time: 55 ms (10.67%), Space: 47.4 MB (91.86%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,16 @@
+class NumArray {
+        public int []arr;
+    public NumArray(int[] nums) {
+        arr=nums;
+    }
+    public int sumRange(int left, int right) {
+        int sum=0;
+        for(int i=left; i<=right; i++){
+           sum+=arr[i];
+            
+            
+        }
+        return sum;
+    }
+}
+
